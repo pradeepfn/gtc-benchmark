bb54f1a3ab8186eaaf20a1db1979daf05fa8fdc7
Pradeep Fernando <pradeepfn@gmail.com>
2015-02-23 11:57:23 -0500

    script file

---
+++ scripts/br.R
@@ -0,0 +1,39 @@
+
+print("Initializing the Rscript")
+#remember initial directory
+initial.dir<-getwd()
+#jump to new one
+print("moving in to /stats dir..")
+setwd("../stats/")
+
+tot_time = 0 
+count = 0
+
+init_time <- numeric()
+populate_status <- numeric()
+compute <- numeric()
+
+regex<-paste('time_*', sep="");
+listfiles <- list.files(path=".",pattern=regex,full.names=TRUE);
+for(filename in listfiles)
+{
+	data <- read.csv(file=filename ,head=FALSE,sep=":")
+	
+	temp <- data$V1[2]-data$V1[1]
+	temp <- temp*1000000 + data$V2[2] - data$V2[1]
+	init_time <- c(init_time,temp)
+
+	temp <- data$V1[3]-data$V1[2]
+	temp <- temp*1000000 + data$V2[3] - data$V2[2]
+	populate_status <- c(populate_status,temp)
+
+	temp <- data$V1[4]-data$V1[3]
+	temp <- temp*1000000 + data$V2[4] - data$V2[3]
+	compute <- c(compute,temp)
+}
+
+print(mean(init_time))
+print(mean(populate_status))
+print(mean(compute))
+setwd(initial.dir);
+
